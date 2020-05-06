e2a433fd936127ce2cfd124bd310c75653fd3f09
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2020-05-06 14:39:04 +0300

    adding auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route } from "react-router-dom";
+
 import './App.css';
+
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import awsExports from "./aws-exports";
+
+import HomePage from './components/HomePage';
+import { createBrowserHistory as createHistory } from 'history'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+
+Amplify.configure(awsExports);
+
+const history = createHistory();
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Navbar bg="primary" expand="lg" variant="dark" >
+          <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <AmplifySignOut />
+        </Navbar>
+        
+       <Route path="/" exact component={HomePage} />
+      </Router>
     </div>
+    </AmplifyAuthenticator>
+    
   );
 }
 
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default HomePage;
+
